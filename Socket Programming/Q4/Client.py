197e15b1d0c7a237819f44775ff3a32c01072ef2
krahulreddy <k_rahul_reddy@outlook.com>
2019-08-07 13:42:23 +0530

    Q4 added

--- Socket Programming/Q4/Client.py
+++ Socket Programming/Q4/Client.py
@@ -1,15 +1,19 @@
-import socket 
-import select 
-import sys 
-  
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+import socket
+import sys
+import select
 
-server.connect(('127.0.0.1', 8090)) 
-  
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.connect(('127.0.0.1', 8090))
+
 while True:
-    message = server.recv(2048) 
-    print message 
-    message = raw_input() 
-    server.send(message) 
-    print "<You>" , message
+    sockets_list, a, b = select.select([sys.stdin, server], [], [])    
+    for s in sockets_list:
+    	if s == server:
+            message = server.recv(2048)
+            print message
+        else:
+            message = raw_input()
+            print "<You>" , message
+            server.send(message)
 server.close()
--- Socket Programming/Q4/Server.py
+++ Socket Programming/Q4/Server.py
@@ -4,44 +4,41 @@ import sys
 from thread import *
 
 def clientthread(conn, addr): 
-  
-    conn.send("Welcome to this chatroom!") 
-  
-    while True: 
-    
-        message = conn.recv(2048) 
-        if message: 
-            print "<" + addr[0] + "> " + message 
-            message_to_send = "<" + addr[0] + "> " + message 
-            broadcast(message_to_send, conn) 
-        else: 
-            remove(conn)   
+    conn.send("Welcome to this chatroom!")
+    while True:
+        message = conn.recv(2048)
+        if message:
+            print "<" + addr[0] + ":" + str(addr[1]) + "> " + message
+            message_to_send = "<" + addr[0] + ":" + str(addr[1]) + "> " + message
+            broadcast(message_to_send, conn)
+        else:
+            remove(conn)
 
-def broadcast(message, connection): 
-    for clients in list_of_clients: 
-        if clients!=connection: 
-            try: 
-                clients.send(message) 
-            except: 
-                clients.close() 
-                remove(clients) 
-def remove(connection): 
-    if connection in list_of_clients: 
+def broadcast(message, connection):
+    for client in list_of_clients:
+        if client != connection:
+            try:
+                client.send(message)
+            except:
+                client.close()
+                remove(client)
+
+def remove(connection):
+    if connection in list_of_clients:
         list_of_clients.remove(connection)
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-  
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 server.bind(('127.0.0.1', 8090))
-  
+
 server.listen(100)
 list_of_clients = []
   
 while True: 
     conn, addr = server.accept()
-    list_of_clients.append(conn) 
-    print addr[0] + " connected"
+    list_of_clients.append(conn)
+    print addr[0] + ":" + str(addr[1]) + " connected"
 
-    start_new_thread(clientthread,(conn,addr))     
-  
+    start_new_thread(clientthread,(conn,addr))
 conn.close() 
 server.close()
